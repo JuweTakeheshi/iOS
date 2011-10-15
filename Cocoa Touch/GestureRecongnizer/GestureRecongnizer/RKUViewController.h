6db6879539b25ac6d22becb8c23adc4f3290e4c0
Juan Karam <juan.karam@me.com>
2011-10-15 13:45:16 -0500

    Clase 5

---
+++ Cocoa Touch/GestureRecongnizer/GestureRecongnizer/RKUViewController.h
@@ -0,0 +1,15 @@
+//
+//  RKUViewController.h
+//  GestureRecongnizer
+//
+//  Created by Juan Jose Karam on 10/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RKUViewController : UIViewController
+
+
+@property(nonatomic, strong) IBOutlet UIView * cuadritoView;
+@end
---
+++ Cocoa Touch/GestureRecongnizer/GestureRecongnizer/RKUViewController.m
@@ -0,0 +1,86 @@
+//
+//  RKUViewController.m
+//  GestureRecongnizer
+//
+//  Created by Juan Jose Karam on 10/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "RKUViewController.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation RKUViewController
+@synthesize cuadritoView = _cuadritoView;
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    _cuadritoView.frame = CGRectMake(CGRectGetMidX(self.view.frame), CGRectGetMidY(self.view.frame) , _cuadritoView.frame.size.width, _cuadritoView.frame.size.height);
+    
+    [self.view addSubview: _cuadritoView];
+    
+    UILongPressGestureRecognizer * longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPressHappened:)];
+    longPress.minimumPressDuration = 2;
+    
+    [_cuadritoView addGestureRecognizer:longPress];
+}
+
+
+- (void)longPressHappened:(UIGestureRecognizer *)recognizer {
+    
+    CABasicAnimation * animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    
+    [animation setToValue:[NSNumber numberWithFloat:0.0]];
+    [animation setFromValue:[NSNumber numberWithFloat:M_PI /16]];
+    
+    [animation setDuration:0.1];
+    [animation setRepeatCount:NSUIntegerMax];
+    [animation setAutoreverses:YES];
+    
+    [_cuadritoView.layer addAnimation:animation forKey:@"sorpresa"];
+    
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return YES;
+}
+
+@end
